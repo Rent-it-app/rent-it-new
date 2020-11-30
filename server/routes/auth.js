1b6fef6fce25b41983a377ea91cedadcce8c0b43
TahanyAljemzawy <tahanyaljemzawy@yahoo.com>
2020-11-30 23:53:22 +0200

    validation on login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -14,7 +14,7 @@ const { schema } = require('../models/User');
 const querySchema = Joi.object({
 
     name         : Joi.string().required(),
-    email        : Joi.string().required().lowercase().email(),
+    email        : Joi.string().required().lowercase().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
     password     : Joi.string().min(8).required().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),//to be mix of these values
     passwordAgain: Joi.ref('password'),//to equal password
 
@@ -22,15 +22,17 @@ const querySchema = Joi.object({
 
 //we add async here cause we need sometime to submit the data here
 router.post('/register',async(req, res)=> {
+
     const { name, email, password, passwordAgain } = req.body;//I love e6 <3 
 
 //*************** */
 //what I need to cheeck for the user registration:
+
 //0- The feileds are empty O.O .. forget this shhhh
     if(!email || !name || !password|| !passwordAgain)
         return res.status(400).json({msg :"Please enter all the information"})
-//1- The email is not alredy at the database
 
+//1- The email is not alredy at the database
 const doesExist = await User.findOne({ email })
 if(doesExist)
 return res.status(400).json({msg :"the email is used"})
@@ -38,8 +40,19 @@ return res.status(400).json({msg :"the email is used"})
  
 
 //2- The inputs are correct using the validation 
-//const testValidate = await querySchema.validateAsync(req.body);
+const{error}=querySchema.validate(req.body);
+if(error){
+    console.log(error)
+    return res.status(400).json({msg :error.details[0].message})}
+
 //console.log(testValidate)
+/*try {
+    const value = await querySchema.validateAsync(name,email,password,passwordAgain);
+}
+catch (err) {
+    res.status(400).json({msg :"3eeeb 3aleek "})
+ }
+*/
    const user = new User({
 
        name         : name,
